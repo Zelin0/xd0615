85de89aab69f08250f19fe0e8e3039dd363fd848
Zelin0 <1315327900@qq.com>
2020-06-19 12:52:19 +0800

    第三次作业提交，张泽霖19020100373

---
+++ lessons3/doubleMotor.c
@@ -0,0 +1,64 @@
+char ch=' ';
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  Serial.println("ok!");
+}
+
+/*
+预定
+ 轮子转时灯泡亮起
+'f':表示前进
+'b':表示后退
+'r':表示两轮朝内转
+'l':表示两轮朝外转
+'s':表示停止
+ 
+*/
+void loop()
+{
+  if(Serial.available()>0)
+  {
+  	ch=Serial.read();
+    switch(ch)
+    {
+    case 'f':
+      digitalWrite(6,HIGH);
+      digitalWrite(7,LOW);
+      digitalWrite(8,HIGH);
+      digitalWrite(9,LOW);
+      break;
+    case 'b':
+      digitalWrite(7,HIGH);
+      digitalWrite(6,LOW);
+      digitalWrite(9,HIGH);
+      digitalWrite(8,LOW);
+      break;
+    case 'l':
+      digitalWrite(7,HIGH);
+      digitalWrite(6,LOW);
+      digitalWrite(8,HIGH);
+      digitalWrite(9,LOW);
+      break;
+    case 'r':
+      digitalWrite(6,HIGH);
+      digitalWrite(7,LOW);
+      digitalWrite(9,HIGH);
+      digitalWrite(8,LOW);
+      break;
+    case 's':
+      digitalWrite(6,LOW);
+      digitalWrite(7,LOW);
+      digitalWrite(9,LOW);
+      digitalWrite(8,LOW);
+      break;
+      
+    default:
+      break;
+    }
+  }
+}
